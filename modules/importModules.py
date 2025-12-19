b1f00933565788de7b5f92f5355d3a3a01a7544d
amaliecjensen <amaliecolstrup@icloud.com>
2025-12-19 15:49:25 +0100

    init

--- modules/importModules.py
+++ modules/importModules.py
@@ -1,6 +1,12 @@
-from intro import calc_price, calc_tax
+from intro import calc_price, calc_tax  # method 1
+import intro  # method 2
 # from intro import *    //this is a bad practice
 
 
+# method1
+
 calc_price()
 calc_tax()
+
+# method2
+intro.calc_tax()
