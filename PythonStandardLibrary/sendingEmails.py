694e050f855b518514b6176e36853e248b5cb60e
amaliecjensen <amaliecolstrup@icloud.com>
2025-12-30 17:31:22 +0100

    init

--- PythonStandardLibrary/sendingEmails.py
+++ PythonStandardLibrary/sendingEmails.py
@@ -1,12 +1,17 @@
 from email.mime.multipart import MIMEMultipart  # multiimportemailextension
 from email.mime.text import MIMEText
 import smtplib
+from pathlib import Path
+from string import Template
+
+template = Template(Path("template.html").read_text())
+
 
 message = MIMEMultipart()
 message["from"] = "amalienovember@gmail.com"
 message["to"] = "test@gmail.com"
 message["subject"] = "this is a test"
-message.attach(MIMEText("body", "html"))
+message.attach(MIMEText(template.substitute({"name": "amalie"}), "html"))
 
 
 with smtplib.SMTP(host="smtp.gmail.com", port=578) as smtp:
